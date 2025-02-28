e5601eaca0f3f5802b3af9ed3a65453075fd45a7
BikramGhart1 <coludingrain@gmail.com>
2025-02-28 20:34:22 +0545

    granted permissions to the db user

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const SECRET_KEY = process.env.SECRET_KEY;
@@ -5,7 +6,7 @@ const queries = require('../database/queries');
 const { validationResult } = require('express-validator');
 const saltRounds = 10;
 
-const formErrHandling = () => {
+const formErrHandling = (req) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
         res.status(400).json({ errors: errors.array() });
@@ -15,7 +16,7 @@ const formErrHandling = () => {
 const getLoginData = async (req, res, next) => {
     const { email, password } = req.body;
     try {
-        formErrHandling();
+        formErrHandling(req);
 
         //sql query to retreive the user from database by email
         const user = await queries.getUserByEmail(email);
@@ -24,9 +25,10 @@ const getLoginData = async (req, res, next) => {
         if (!isPasswordValid) {
             return res.status(400).json({ message: "Either password or email is invalid" });
         }
-        const token = jwt.sign({ email: tempDB.email }, SECRET_KEY, { expiresIn: "24h" });
+        const token = jwt.sign({ email: user.email }, SECRET_KEY, { expiresIn: "24h" });
         res.status(200).json({ token, message: "Login successful" });
     } catch (err) {
+        console.error("Error in logindata: ",err);
         res.status(500).json({ message: "Server error" });
     }
 }
@@ -34,8 +36,8 @@ const getLoginData = async (req, res, next) => {
 const signUpCreateUser = async (req, res, next) => {
     try {
         //error handling for form validation
-        formErrHandling();
-
+        formErrHandling(req);
+     
         //get input fields datas from frontend and hash the password
         const { email, password, username } = req.body;
         const hashedPassword = await bcrypt.hash(password, saltRounds);
@@ -44,6 +46,7 @@ const signUpCreateUser = async (req, res, next) => {
         const user = await queries.createUser(email, username, hashedPassword);
         res.status(200).json({ user, message: "Account created successfully" });
     } catch (err) {
+        console.error("Error in signupCreateUser: ", err);
         res.status(500).json({ message: "Server error" });
     }
 }
