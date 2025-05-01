dcb751ed9e89f412e1e6d8aa7951ea69f655ee20
BikramGhart1 <coludingrain@gmail.com>
2025-05-01 07:26:53 +0545

    env variables config

--- database/db.js
+++ database/db.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
+    user: process.env.DB_USER || 'apputilizer',
+    host: process.env.DB_HOST || 'localhost',
+    database: process.env.DB_NAME || 'dharanas',
     password: process.env.DB_PASSWORD,
     port: process.env.DB_PORT
 });
